fe1c249caf756b9440cbe75305e24c9dd0248d17
ojengdong <dutkdgus159@naver.com>
2023-06-28 20:28:40 +0900

    dd

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCh8upcN6T2oFjbs9tpTyKUWz-UTKGmwS0",
+    authDomain: "twitter-4c058.firebaseapp.com",
+    projectId: "twitter-4c058",
+    storageBucket: "twitter-4c058.appspot.com",
+    messagingSenderId: "1054742464834",
+    appId: "1:1054742464834:web:bc2b6167c2b3c5edfea0db"
+  };
+
+export default initializeApp(firebaseConfig);
