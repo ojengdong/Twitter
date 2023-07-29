2990cc9f9411a45c1af14406672e174f7ffbaaa5
ojengdong <dutkdgus159@naver.com>
2023-07-29 23:10:52 +0900

    에러해결

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore";
 import "firebase/auth"
-import "firebase/database"
 
 const firebaseConfig = { 
     apiKey: process.env.REACT_APP_API_KEY,
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,42 +1,44 @@
-import React, { useEffect } from 'react'
-import { authService } from '../firebase';
-import { getAuth } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import {dbService} from 'firebase';
+import React, { useEffect } from "react";
+import { authService } from "../firebase";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
-    const auth = getAuth();
-    const navigate = useNavigate();
-    const [nweet, setNweet] = useState("");
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [nweet, setNweet] = useState("");
 
-    useEffect(() => {
-        if (auth.currentUser == null) {
-            navigate("/auth");
-        }
-    }, [])
+  useEffect(() => {
+    if (auth.currentUser == null) {
+      navigate("/auth");
+    }
+  }, []);
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        dbService.collection("nweets").add({
-            
-        });
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const onChange = (event) => {
-        // event 안에 있는 target안에 있는 value를 달라고 하는것
-        const {
-            target : {value},
-        } = event;
-        setNweet(value);
-    };
-    
-    return (
-        <form action="" onSubmit={onSubmit}>
-            <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
-            <input type="submit" value="Nweet" />
-        </form>
-    )
-} 
+  const onChange = (event) => {
+    // event 안에 있는 target안에 있는 value를 달라고 하는것
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <form action="" onSubmit={onSubmit}>
+      <input
+        value={nweet}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Nweet" />
+    </form>
+  );
+};
 
 export default Home;
