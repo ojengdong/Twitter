769820ae290b4191c9e5ac37d8b264d2dd1d60ba
ojengdong <dutkdgus159@naver.com>
2023-08-08 18:12:20 +0900

    dd

--- src/firebase.js
+++ src/firebase.js
@@ -16,4 +16,4 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
 
   export const authService = getAuth();
-  export const firestore = getFirestore();
+  export const dbService = getFirestore();
