6bcc4009cc1abb9f33b4534e4500069465853e2d
ojengdong <dutkdgus159@naver.com>
2023-08-08 18:49:27 +0900

    firestore 해결

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { authService, dbService } from "../firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -15,20 +16,21 @@ const Home = () => {
     }
   }, []);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     // .add : data가 들어감 즉 명시된 데이터를 담은 새로운document을 collection 추가 document ID 를 자동으로 부여하면서
     // dbService.collection("nweets") 이 부분을 사용해서 파이어베이스의 "nweets" 컬렉션에 접근한다
     // dbService.collection("nweets").addDoc()를 호출하여 새로운 문서를 해당 컬렉션에 추가하고
     // 자동으로 생성된 ID를 사용하게 된다.
-    // addDoc()메소드의 전달되는 파이어베이스에 저장되는 데이터를 포함한다.
-    dbService.collection("nweets").addDoc({
-      // createdAt은 코드 실행 시점의 타임스탬프로, 타임스탬프 값이 데이터에 추가 된다.
+    // addDoc()메소드의 전달되는 파이어베이스에 저장되는 데이터를 포함한다. 
+    // createdAt은 코드 실행 시점의 타임스탬프로, 타임스탬프 값이 데이터에 추가 된다.
       // 이렇게 입력한 텍스트와 작성된 시간을 함께 파이어베이스에 저장할 수 있다.
       // setNweet("")를 호출하여 입력 필드를 초기화 하고 이렇게 하면 다음 nweet을 작성할 때
       // 빈 문자열로 시작하게 된다.
+      
+    await addDoc(collection(dbService, "nweets"), {
       nweet,
-      createdAt: Date.now(),
+      createdAt: serverTimestamp(),
     });
     setNweet("");
   };
